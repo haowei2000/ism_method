189b0fc3ad1abba1bde5817b32072cc9fb731516
Haowei Wang <hw_wang@whu.edu.cn>
2024-11-24 17:17:31 +0800

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit f5fb0b88d44e113d29f721490bba0aafbf03e6ce, reversing
    changes made to 83e8d606b4b3c5ed327d122e3d25a41f97f377e8.

--- src/ism/main.py
+++ src/ism/main.py
@@ -1,17 +1,17 @@
 import logging
-from ism_method.get_method import get_method
-from ism_method.mongodb import get_collection
+from get_method import get_method
+from mongodb import get_collection
 from tqdm import tqdm
 
 
 def main():
     # 获取集合
     collection = get_collection()
-    # 删除集合中Method-研究方法字段不存在的记录
-    collection.delete_many({"Method-研究方法": {"$exists": False}})
+
     # 查找所有记录（示例中假设返回多个记录）
     records = collection.find({"Summary-摘要": {"$exists": True}}, {"_id": 1, "Summary-摘要": 1, "Method-研究方法": 1})
     count = collection.count_documents({})
+    
     pbar = tqdm(desc="Processing records", total=count)
     for record in records:
         # 提取方法
@@ -28,7 +28,7 @@ def main():
                 )
                 pbar.update(1)
             except Exception as e:
-                logging.error(f"Error processing record: {record['_id']} {e}")
+                logging.error(f"Failed to process record {record}: {e}")
                 continue
     pbar.close()
 
